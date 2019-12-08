af126ef5d8b589985d88e493d6c514f9f4f5cb06
akmatoff <azim.akmatov1@gmail.com>
2019-12-08 17:56:22 +0600

    Initial commit

---
+++ app/src/main/java/com/coorootinc/englishquiz/DBHelper.java
@@ -0,0 +1,144 @@
+package com.coorootinc.englishquiz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME  = "QuizApp.db";
+    private static final int DATABASE_VERSION = 6;
+
+    private SQLiteDatabase db;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuizContract.QuestionsTable.TABLE_NAME + " ( " +
+                QuizContract.QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuizContract.QuestionsTable.COLUMN_QUESTION + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_OPTION4 + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_ANSWER_NUM + " INTEGER" +
+                ")";
+
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionsTable();
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + QuizContract.QuestionsTable.TABLE_NAME);
+        onCreate(db);
+
+    }
+
+    private void fillQuestionsTable() {
+        Question q1 = new Question("Основу древнего римского народа составляли", "римляне, сабины, эллины", "римляне, греки, этруски ", "римляне, сабины, этруски", "римляне, сабины, славяне", 3);
+        addQuestion(q1);
+        Question q2 = new Question("Языком римской цивилизации становится", "арабский", "римский", "греческий", "латынь", 4);
+        addQuestion(q2);
+        Question q3 = new Question("Кого римляне называли «варварами»?", "племена не знающие латинского или греческого языка", "жестоких императоров", "всех, кто проживал вне города", "всех", 1);
+        addQuestion(q3);
+        Question q4 = new Question("На чем писались древнерусские письма , документы?", "Береста", "Папирус", "Пергамент", "Лыко", 1);
+        addQuestion(q4);
+        Question q5 = new Question("Как в Древней Руси назывался княжеский престол?", "Стол", "Седалище ", "Держава", "Стуло", 1);
+        addQuestion(q5);
+        Question q6 = new Question("Начиная с какого времени в Китае начала править династия Тан?", " С начала седьмого века", "С начала шестого века ", "С конца восьмого века ", "С конца седьмого века", 1);
+        addQuestion(q6);
+        Question q7 = new Question("Какие кочевники начали завование Китая в начале XIII века?", "Скифы", "Тюрки", "Монголы", "Авары ", 3);
+        addQuestion(q7);
+        Question q8 = new Question("Столица монгольской державы", "Самарканд", "Пекин", "Сарай-Бату", "Каракорум", 3);
+        addQuestion(q8);
+        Question q9 = new Question("Что было изобретено в средневековом Китае?", "Печатный станок ", "Изготовление фарфора", "Боевая колесница", "Все варианты верны", 2);
+        addQuestion(q9);
+        Question q10 = new Question("В период какой династии средневековый Китай достиг наибольших размеров?", "Мин", "Тан", "Цинь", "Сун", 2);
+        addQuestion(q10);
+        Question q11 = new Question("Сколько длились темные века?", "XII - IX", "XI - IX", "XI - X", "XII - X", 2);
+        addQuestion(q11);
+        Question q12 = new Question("События в 1194 - 1184 до н. э.", "Вторжение на Пелопоннес дорийских племен", "Нет правильного ответа", "Троянская война", "Вторжение на Пелопоннес дорийских племен", 3);
+        addQuestion(q12);
+        Question q13 = new Question("Поход Дария I против скифов.", "514 г.", "524 г.", "510 г.", "526 г.", 1);
+        addQuestion(q13);
+        Question q14 = new Question("В каком году открыли олимпийские игры?", "866 г.", "776 г.", "756 г.", "856 г.", 2);
+        addQuestion(q14);
+        Question q15 = new Question("Правление Дария I в Персии", "522-486 гг.", "552-468 гг.", "520-480 гг.", "523-481 гг.", 1);
+        addQuestion(q15);
+        Question q16 = new Question("Год формирования афинской морской державы", "Нет правильного ответа", "454 г.", "554 г.", "523 г.", 2);
+        addQuestion(q16);
+        Question q17 = new Question("Война Спарты с Персией", "389-394 гг.", "400-395 гг.", "399-394 гг.", "390-394 гг.", 3);
+        addQuestion(q17);
+        Question q18 = new Question("Египетский поход Александра", "330-331 гг.", "Нет правильного ответа", "331-333 гг.", "331-342 гг.", 2);
+        addQuestion(q18);
+        Question q19 = new Question("Годы жизни философа Сократа", "399-467 гг.", "399-347 гг.", "399-470 гг.", "399-481 гг.", 3);
+        addQuestion(q19);
+        Question q20 = new Question("Годы жизни философа Платона", "345-425 гг.", "347-427 гг.", "346-426 гг.", "346-430 гг.", 2);
+        addQuestion(q20);
+        Question q21 = new Question("Определите среди перечисленных город, который не располагался на территории Древнего Египта", "Абидос", "Сидон", "Саис", "Сиена", 2);
+        addQuestion(q21);
+        Question q22 = new Question("Когда в Римской империи были запрещены Олимпийские игры, посвященные Зевсу и с чем это было связано?", "в IV в. до н.э., с провозглашением Римской республики", "в I в. до н.э.,с восстанием Спартака", "в III в. н.э., с началом распада империи и постоянными войнами", "в IV в. н.э., с утверждением христианской религии в Риме", 4);
+        addQuestion(q22);
+        Question q23 = new Question("В низовьях каких рек были основаны древнегреческие колонии?", "Тигра, Нила, По", "По, Дона, Нила", "Днепра, Дуная, Евфрата", "Днестра, Днепра, Тигра", 2);
+        addQuestion(q23);
+        Question q24 = new Question("В каком из государств функционировала так называемая Царская дорога?", "древнем Риме", "Ассирийском", "древнем Египте", "Персидском", 4);
+        addQuestion(q24);
+        Question q25 = new Question("В каком ответе хронологически правильно указана последовательность событий древней истории?", "изготовление керамики, изобретение лука и стрел", "появление наскальных рисунков, первые захоронения умерших", "домостроительство, захоронение умерших", "первые наскальные рисунки, изобретение лука и стрел", 4);
+        addQuestion(q25);
+        Question q26 = new Question("Определите основные занятия древних людей, живших 30-14 тыс. лет тому назад.", "охота, собирательство", "охота, собирательство, рыболовство", "собирательство, приручение животных, мотыжное земледелие", "ремесло, земледелие, скотоводство", 2);
+        addQuestion(q26);
+        Question q27 = new Question("Когда был построен храм бога Луны в городе Уре?", "в XIX в. до н.э.", "в XX в. до н.э.", "в XXI в. до н.э.", "в XVIII в. до н.э", 3);
+        addQuestion(q27);
+        Question q28 = new Question("Одной из наиболее почитаемых богинь Греции была богиня плодородия Деметра, а у римлян богиней плодородия была ...", "Минерва", "Диана", "Юнона", "Церера", 4);
+        addQuestion(q28);
+        Question q29 = new Question("Покровителем виноделия в Греции был бог Дионис, о котором говорится, что его всегда сопровождали неизменные спутники сатиры и спутницы ...", "менады", "нимфы", "русалки", "музы", 1);
+        addQuestion(q29);
+        Question q30 = new Question("Кто из античных скульпторов создал прославленную статую Зевса Олимпийского?", "Поликлет", "Фидий", "Мирон", "Гесиод", 2);
+        addQuestion(q30);
+    }
+
+    private void addQuestion(Question question) {
+        ContentValues cv = new ContentValues();
+        cv.put(QuizContract.QuestionsTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuizContract.QuestionsTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuizContract.QuestionsTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuizContract.QuestionsTable.COLUMN_OPTION3, question.getOption3());
+        cv.put(QuizContract.QuestionsTable.COLUMN_OPTION4, question.getOption4());
+        cv.put(QuizContract.QuestionsTable.COLUMN_ANSWER_NUM, question.getAsnwerNum());
+        db.insert(QuizContract.QuestionsTable.TABLE_NAME, null, cv);
+    }
+
+    public List<Question> getAllQuestions() {
+        List<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + QuizContract.QuestionsTable.TABLE_NAME, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setQuestion(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION3)));
+                question.setOption4(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION4)));
+                question.setAsnwerNum(c.getInt(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_ANSWER_NUM)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return questionList;
+    }
+}
---
+++ app/src/main/java/com/coorootinc/englishquiz/QuizActivity.java
@@ -0,0 +1,229 @@
+package com.coorootinc.englishquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class QuizActivity extends AppCompatActivity {
+    public static final String EXTRA_SCORE = "extraScore";
+    private static final long COUNTDOWN_IN_MILLIS = 30000;
+
+    private TextView textViewQuestion;
+    private TextView textViewScore;
+    private TextView textViewQuestionCount;
+    private TextView textViewCountDown;
+    private RadioGroup rbGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private RadioButton rb4;
+    private Button buttonConfirmNext;
+
+    private ColorStateList textColorDefaultRb;
+    private ColorStateList textColorDefaultCd;
+
+    private CountDownTimer countDownTimer;
+    private long timeleftInMillis;
+
+    private List<Question> questionList;
+    private int questionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+
+    private int score;
+    private boolean answered;
+
+    private long backPressedTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        textViewQuestion = findViewById(R.id.question_text);
+        textViewScore = findViewById(R.id.score_text);
+        textViewQuestionCount = findViewById(R.id.question_number);
+        textViewCountDown = findViewById(R.id.countdown_text);
+
+        rbGroup = findViewById(R.id.radio_group);
+        rb1 = findViewById(R.id.radio_button1);
+        rb2 = findViewById(R.id.radio_button2);
+        rb3 = findViewById(R.id.radio_button3);
+        rb4 = findViewById(R.id.radio_button4);
+        buttonConfirmNext = findViewById(R.id.confirm_button);
+
+        textColorDefaultRb = rb1.getTextColors();
+        textColorDefaultCd = textViewCountDown.getTextColors();
+
+        final com.coorootinc.englishquiz.DBHelper dbHelper = new com.coorootinc.englishquiz.DBHelper(this);
+        questionList = dbHelper.getAllQuestions();
+        questionCountTotal = questionList.size();
+        Collections.shuffle(questionList);
+
+        showNextQuestion();
+
+        buttonConfirmNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!answered) {
+                    if (rb1.isChecked() || rb2.isChecked() || rb3.isChecked() || rb4.isChecked()) {
+                        checkAnswer();
+                    } else {
+                        Toast.makeText(QuizActivity.this, "Пожалуйста, выберите вариант ответа", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    showNextQuestion();
+                }
+            }
+        });
+
+    }
+
+    private void showNextQuestion() {
+        rb1.setTextColor(textColorDefaultRb);
+        rb2.setTextColor(textColorDefaultRb);
+        rb3.setTextColor(textColorDefaultRb);
+        rb4.setTextColor(textColorDefaultRb);
+        rbGroup.clearCheck();
+
+        if (questionCounter < questionCountTotal) {
+            currentQuestion = questionList.get(questionCounter);
+
+            textViewQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+            rb3.setText(currentQuestion.getOption3());
+            rb4.setText(currentQuestion.getOption4());
+
+            questionCounter++;
+            textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
+            answered = false;
+            buttonConfirmNext.setText("CONFIRM");
+
+            timeleftInMillis = COUNTDOWN_IN_MILLIS;
+            startCountDown();
+        } else {
+            finishQuiz();
+        }
+    }
+
+    private void startCountDown() {
+        countDownTimer = new CountDownTimer(timeleftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeleftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timeleftInMillis = 0;
+                updateCountDownText();
+                checkAnswer();
+            }
+        }.start();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (timeleftInMillis / 1000) / 60;
+        int seconds = (int) (timeleftInMillis / 1000) % 60;
+
+        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        textViewCountDown.setText(timeFormatted);
+
+        if (timeleftInMillis < 10000) {
+            textViewCountDown.setTextColor(Color.RED);
+        } else {
+            textViewCountDown.setTextColor(textColorDefaultCd);
+        }
+    }
+
+    private void checkAnswer() {
+        answered = true;
+
+        countDownTimer.cancel();
+
+        RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
+        int answerNum = rbGroup.indexOfChild(rbSelected) + 1;
+
+        if (answerNum == currentQuestion.getAsnwerNum()) {
+            score++;
+            textViewScore.setText("Score: " + score);
+        }
+
+        showSolution();
+    }
+
+    private void showSolution() {
+        rb1.setTextColor(Color.RED);
+        rb2.setTextColor(Color.RED);
+        rb3.setTextColor(Color.RED);
+        rb4.setTextColor(Color.RED);
+
+        switch (currentQuestion.getAsnwerNum()) {
+            case 1:
+                rb1.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 1 is correct!");
+                break;
+            case 2:
+                rb2.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 2 is correct!");
+                break;
+            case 3:
+                rb3.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 3 is correct!");
+                break;
+            case 4:
+                rb4.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 4 is correct!");
+                break;
+        }
+
+        if (questionCounter < questionCountTotal) {
+            buttonConfirmNext.setText("NEXT");
+        } else {
+            buttonConfirmNext.setText("FINISH");
+        }
+    }
+
+    private void finishQuiz() {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(EXTRA_SCORE, score);
+        setResult(RESULT_OK, resultIntent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            finishQuiz();
+        } else {
+            Toast.makeText(this, "Press one more time to exit!", Toast.LENGTH_SHORT).show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+    }
+}
